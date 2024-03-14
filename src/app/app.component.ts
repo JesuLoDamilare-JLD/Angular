100beaf7d1dfb23c4a98bbb9814bf98c56d7135e
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-14 23:18:10 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Hello-world';
+  title = 'Dare first Angular - And really I cant believe its working';
 }
