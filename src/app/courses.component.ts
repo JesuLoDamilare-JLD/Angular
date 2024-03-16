f376919165ca434ad4b1deb247d36898400b13eb
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-16 23:29:15 +0000

    ok

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'courses', // <courses>
+  selector: 'courses',
+  template: '<h2>Courses</h2>',
 })
 export class CoursesComponent {}
